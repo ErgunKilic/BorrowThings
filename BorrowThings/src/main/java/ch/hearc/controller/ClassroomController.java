45f2ecee707d1a1a82c6ecac025a8373ce0c3da8
ergun.kilic <ergun.kilic@he-arc.ch>
2020-04-09 13:52:06 +0200

    Some crud update for item and classroom

--- BorrowThings/src/main/java/ch/hearc/controller/ClassroomController.java
+++ BorrowThings/src/main/java/ch/hearc/controller/ClassroomController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
 import ch.hearc.model.*;
 
 @Controller
@@ -16,21 +19,41 @@ public class ClassroomController {
     @Autowired
     ClassroomRepository classroomRepository;
 
+    // GET ALL
     @GetMapping("/classroom/all")
     public String getAll(Map<String, Object> model) {
         model.put("classrooms", classroomRepository.findAll());
         return "classroom/classrooms";
     }
 
+    // CREATE
     @GetMapping("/classroom/create")
-    public String roleForm(Model model) {
-        model.addAttribute("classrom", new Classroom());
+    public String classroomForm(Model model) {
+        model.addAttribute("classroom", new Classroom());
         return "classroom/classroom-form";
     }
 
     @PostMapping("/classroom/insert")
-    public String insertRole(@ModelAttribute Classroom classroom, Model model) {
+    public String insertClassroom(@ModelAttribute Classroom classroom, Model model) {
         classroomRepository.save(classroom);
         return "classroom/classroom-form";
+    }
+
+
+    // Update
+
+    @GetMapping("/classroom/{id}")
+    public String usersAll(Model model, @PathVariable(value = "id") int id) {
+        Classroom classroom = classroomRepository.findById(id).get();
+        model.addAttribute("classroom", classroom);
+        return "classroom/classroom-update";
+    }
+
+    @PostMapping("/classroom/update")
+    public RedirectView userUpdate(@ModelAttribute Classroom classroom, Model model) {
+        Classroom classroomChange = classroomRepository.findById(classroom.getId()).get();
+        classroomChange.setName(classroom.getName());
+        classroomRepository.save(classroomChange);
+        return new RedirectView("/classroom/all");
     }
 }
--- BorrowThings/src/main/java/ch/hearc/model/ClassroomRepository.java
+++ BorrowThings/src/main/java/ch/hearc/model/ClassroomRepository.java
@@ -2,6 +2,6 @@ package ch.hearc.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
 
 }
